0d3fcce46df5ec5b0350bcf762866d410b86849b
quainialberto <alberto_quaini@hotmail.it>
2018-07-22 18:07:58 -0500

    commit

---
+++ albi_sol/day3_hw/omp_day3/pi_MC_omp_speedup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from matplotlib import pyplot as plt
+import numpy as np
+
+seconds = np.array([
+    54.537497121,
+    33.606769538,
+    18.503835911,
+    10.816416261,
+    5.701376772
+    ])
+threads = np.array([
+    1,
+    2,
+    4,
+    8,
+    16
+    ])
+seconds = seconds / seconds[0]
+print(seconds)
+plt.plot(threads, seconds, 'o')
+plt.plot(threads, seconds)
+plt.title('DSDP OMP speedup')
+plt.xlabel('threads')
+plt.ylabel('seconds')
+plt.show()
+
+plt.savefig('pi_MC_OMP_speedup.png')
